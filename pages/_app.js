6941cd89c0f988f08201441433681f03106adfeb
ekeemmanuel <emmazy100@gmail.com>
2025-10-28 02:17:24 +0100

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import { AuthProvider } from "../contexts/AuthContext";
 import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta
           name="description"
           content="Essential safety guides, legal rights information, and health resources for sex workers."
@@ -15,7 +16,32 @@ function MyApp({ Component, pageProps }) {
           content="https://meetanescortblog.vercel.app/logo.png"
         />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
+
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_US",
+          url: "https://meetanescort.info",
+          site_name: "MeetAnEscort",
+          title: "MeetAnEscort - Safety Resources for Sex Workers",
+          description:
+            "Essential safety guides, legal rights information, and health resources for sex workers.",
+          images: [
+            {
+              url: "https://meetanescortblog.vercel.app/logo.png",
+              width: 1200,
+              height: 630,
+              alt: "MeetAnEscort Logo",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@meetanescort",
+          site: "@meetanescort",
+          cardType: "summary_large_image",
+        }}
+      />
 
       <AuthProvider>
         <Component {...pageProps} />
